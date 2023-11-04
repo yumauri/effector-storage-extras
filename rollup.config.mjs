7dba0811e2fc55b0b59904572e339f17b3fba4e1
Victor Didenko <victord@setplex.com>
2023-11-04 12:04:22 +0300

    Bump minimal effector version to 22.4.0, drop 22.0.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -94,7 +94,7 @@ const src = () => ({
           ROOT_PACKAGE_JSON.homepage + '/tree/main/packages/' + WORKSPACE_NAME,
         keywords: [...ROOT_PACKAGE_JSON.keywords, ...pkg.keywords],
         peerDependencies: {
-          effector: '>=22.0.0',
+          effector: '>=22.4.0',
         },
 
         // cjs + esm magic
