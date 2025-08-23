e85b6f1c9ef42a32fd90d29810a78f3da24b416c
yumauri <yumaa.verdin@gmail.com>
2025-08-23 10:19:50 +0300

    Replace prettier and eslint with biome

--- types/effector-storage/index.d.ts
+++ types/effector-storage/index.d.ts
@@ -10,6 +10,7 @@ import type {
 // override effector-storage's `Persist` interface to make it more loose
 declare module 'effector-storage' {
   export interface Persist {
+    // biome-ignore lint/style/useShorthandFunctionType: this has to be interface for declaration merging
     <State, Err = Error>(
       config: Partial<
         (ConfigAdapter | ConfigAdapterFactory<any>) &
